095924d438c8b9d97fa4bdda3a5ef997e366f10b
Thomas Coradetti <tmcoradetti@gmail.com>
2020-07-28 16:55:21 -0400

    added Employee class constructor info

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -5,7 +5,7 @@ test("Can instantiate Employee instance", () => {
   expect(typeof(e)).toBe("object");
 });
 
-test.only("Can set name via constructor arguments", () => {
+test("Can set name via constructor arguments", () => {
   const name = "Alice";
   const e = new Employee(name);
   expect(e.name).toBe(name);
